3eb384862e32aeb6c47bde1fc20db133134c15f3
ArhurBenk <arthur.benk.ifc@gmail.com>
2025-04-22 08:50:41 -0300

    Feat: icons

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,18 @@ const filmes = ref([
 </script>
 
 <template>
+<head>
+</head>
+  
+
   <h1>Hello World</h1>
   <article v-for="catalago in filmes" :key="catalag.id">
       <h3>{{  }}</h3>
       <p>{{  }}</p>
     </article>
+   
 </template>
 
 <style scoped>
 
 </style>
-
--- :class="'artigo-' + noticia.id" class="card"
