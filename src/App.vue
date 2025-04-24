038bbfb4dd819ca90c234d65284dc32383bfea57
youtario <ijairerick.info1.ifc@gmail.com>
2025-04-24 09:10:33 -0300

    FEAT:  Começo e termino da footer

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const filmes = ref([
 </script>
 
 <template>
-  <div class="cabeçario">
+   <header>
     <h3>IFbooks</h3>
     <p>apreço e leitura</p>
     <input type="text" id="searchInput" placeholder="Pesquisar">
@@ -19,13 +19,37 @@ const filmes = ref([
       <li>Envio</li>
       <li>Devoluções</li>
     </ul>
-      
-  </div>
+    </header>
   <div>
     <p>Autor de Abril</p>
     <h1>Eric-Emanuel Schmitt</h1>
     <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
     <button>Acessar página do livro</button>
+    <p>Frete grátis para SC</p>
+    <p>Livros recomendados</p>
+    <p>Mais vendidos</p>
+  </div>
+  <div class="main">
+    <h3>Lançamento</h3>
+  </div>
+  <div class="footer">
+    <footer>
+      <p>IFbooks</p>
+      <img src="Images/raphael_facebook.png" alt="facebook">
+      <img src="Images/Vector(1).png" alt="instagram">
+      <img src="Images/fa6-brands_square-twitter.png" alt="twitter">
+      <p>Contato</p>
+      <img src="Images/Icon.png" alt="Telefone">
+      <p>+55 47 40045263</p>
+      <img src="Images/Clock.png" alt="Relógio">
+      <p>8h às 23h - Seg a Sex</p>
+      <img src="Images/Mail.png" alt="Email">
+      <p>contato@ifbooks.com</p>
+      <img src="Images/paipalCard-Logo.png" alt="PayPal">
+      <img src="Images/MasterCard-Logo.png" alt="MasterCard">
+      <img src="Images/VISA-card-logo.png" alt="Visa">
+      <p>&copy; Alguns direitos reservados. IFbooks 2025.</p>
+    </footer>
   </div>
   <h1>Hello World</h1>
   <article v-for="catalago in filmes" :key="catalag.id">
