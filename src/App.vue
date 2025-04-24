fcf051f03a36006bdf6b739513446d16d1d37a44
ArhurBenk <arthur.benk.ifc@gmail.com>
2025-04-24 09:29:30 -0300

    Feat: css header

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,16 @@ const filmes = ref([
     <h3>IFbooks</h3>
     <p>apreço a <br> leitura</p>
     <input type="text" id="searchInput" placeholder="Pesquisar">
-    <button onclick="pesquisar()">Pesquisar</button>
+    <button onclick="pesquisar()"><img src="../src/Images/icons/procurar.png" alt=""></button>
     <ul>
       <li>Termos</li>
       <li>Equipe</li>
       <li>Envio</li>
-      <li>Devoluções</li>
+      <span><li>Devoluções</li></span>
     </ul>
-    <img src="../src/Images/icons/carrinho-de-compras.png" alt="">
+    <span><a href="#"><img src="../src/Images/icons/carrinho-de-compras.png" alt=""></a></span>
+    <span><a href="#"><img src="../src/Images/icons/coracao.png" alt=""></a></span>
+    <a href="#"><img src="../src/Images/icons/do-utilizador.png" alt=""></a>
       
   </header>
   <div>
@@ -37,16 +39,65 @@ const filmes = ref([
 </template>
 
 <style scoped>
+  template{
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  }
   header{
     display: flex;
+    border-bottom: #27AE60 1.5px solid;
+  }
+  header h3{
+    padding: 5px 0 5px 250px;
+    margin: 20px 10px 0 15px; 
+  }
+  header p{
+    color: #27AE60;
+    margin: 20px 75px 23px 0;
+    padding-left: 10px;
+    font-weight: 300;
+    line-height: 20px;
+    border-left: #27AE60 2px solid;  
   }
   header ul{
     display: flex;
+  }
+  header input{
+    background-color: #F1F1F1;
+    border: none;
+    outline: none;
+    margin-top: 23px;
+    padding: 3px 3px 3px 18px;
+    height: 2vw;
+    width: 20vw;
+    font-size: 1rem;
+    
+  }
+  header button{
+    border: none;
+    background-color: #F1F1F1;  
+    height: 2vw;
+    margin-top: 23px;
+    padding: 0 15px 0 15px
+  }
+  header button img{
+    width: 20px;
+    opacity: 50%;
   }
   header ul li{
     padding: 30px;
     list-style: none;
     color: #7B7881;
   }
-
+  header ul span li {
+    padding-right: 60px;
+  }
+  header a img{
+    width: 63px;
+    margin-top: 30px;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+  header span img{
+    border-right: #27AE60 1px solid;
+  }
 </style>
