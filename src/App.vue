c3020d72eff07f44cf870ab9b02d496129b530bc
ArhurBenk <arthur.benk.ifc@gmail.com>
2025-04-28 11:56:19 -0300

    feat: banner

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ const filmes = ref([
 </script>
 
 <template>
+
   <header>
     <h3>IFbooks</h3>
     <p>apreço a <br> leitura</p>
@@ -22,20 +23,21 @@ const filmes = ref([
     <span><a href="#"><img src="../src/Images/icons/carrinho-de-compras.png" alt=""></a></span>
     <span><a href="#"><img src="../src/Images/icons/coracao.png" alt=""></a></span>
     <a href="#"><img src="../src/Images/icons/do-utilizador.png" alt=""></a>
-      
   </header>
-  <div>
-    <p>Autor de Abril</p>
+
+
+  <div id="banner">
+    <div id="text">
+    <span><p>Autor de Abril</p></span>
     <h1>Eric-Emanuel Schmitt</h1>
-    <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+    <p>Eric-Emmanuel Schmitt has been awarded more than 20<br>literary prizes and distinctions, and in 2001 he received the<br>title of Chevalier des Arts et des Lettres. His books have been<br>translated into over 40 languages.</p>
     <button>Acessar página do livro</button>
   </div>
-  <h1>Hello World</h1>
-  <article v-for="catalago in filmes" :key="catalag.id">
-      <h3>{{  }}</h3>
-      <p>{{  }}</p>
-    </article>
-   
+  <div id="image">
+    <img src="../src/Images/book.png" alt="">
+  </div>
+   </div>
+  
 </template>
 
 <style scoped>
@@ -44,8 +46,9 @@ const filmes = ref([
   }
   header{
     display: flex;
-    border-bottom: #27AE60 1.5px solid;
+    border-bottom: #27AE60 2px solid;
   }
+
   header h3{
     padding: 5px 0 5px 250px;
     margin: 20px 10px 0 15px; 
@@ -99,5 +102,42 @@ const filmes = ref([
   }
   header span img{
     border-right: #27AE60 1px solid;
+  }
+  div#banner {
+    display: flex;
+    padding: 0 200px 10px 300px;
+    border-bottom: #27AE60 2px solid;
+  }
+  div#banner div#text {
+    padding-top: 150px;
+  }
+  div#banner div#text span p{
+    color: #27AE60;
+    border: #27AE60 1px solid;
+    border-radius: 5%;
+    padding: 10px;
+    margin: 0 350px 0 0;
+  }
+  div#banner div#text p{
+    color:#4D4C4C ;
+    padding-bottom: 40px;
+  }
+  div#banner div#text h1{
+    padding: 15px 0 20px 0;
+    font-size: 3rem;
+    font-weight: 500;
+    color: #382C2C;
+  }
+  
+  div#banner div#text button{
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    background-color: #27AE60;
+    border: none;
+    border-radius: 3%;
+  }
+  div#banner div#image img{
+    margin: 20px 0 0 350px;
   }
 </style>
